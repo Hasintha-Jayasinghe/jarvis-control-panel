5d79ef59713060422db80956c96e858641554cf7
Hasintha Jayasinghe <hasinthajayasinghe07@gmail.com>
2021-06-01 04:13:06 +0530

    Apparently I had the routes mixed up. So I switched them up

--- src/Controls/lamp.ts
+++ src/Controls/lamp.ts
@@ -4,14 +4,14 @@ import i2c from 'i2c-bus';
 const lampRouter = express.Router();
 // ENTER A PORT!
 
-lampRouter.get('/on', (_, res) => {
+lampRouter.get('/off', (_, res) => {
   const lamp = i2c.openSync(1);
   lamp.writeByteSync(0x8, 0, 1);
 
   res.send({ lamp: 'on' });
 });
 
-lampRouter.get('/off', (_, res) => {
+lampRouter.get('/on', (_, res) => {
   const lamp = i2c.openSync(1);
   lamp.writeByteSync(0x8, 0, 0);
 
